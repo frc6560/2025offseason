515298ca2d42d1d27c9bf2f70c8c2a66b46c39cb
kevin chou <Kevinurmomchou@icloud.com>
2025-10-13 00:23:53 -0700

    outtake

---
+++ src/main/java/frc/robot/commands/Outtake.java
@@ -0,0 +1,61 @@
+package frc.robot.commands;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import edu.wpi.first.math.Pair;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
+//import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.Constants;
+import frc.robot.Constants.ElevatorConstants;
+import frc.robot.Constants.DrivebaseConstants;
+import frc.robot.Constants.ArmConstants;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.subsystems.swervedrive.*;
+import frc.robot.subsystems.Elevator;
+import frc.robot.subsystems.Arm;
+import frc.robot.subsystems.BallGrabber;
+import frc.robot.ManualControls;
+import java.util.Optional;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import java.util.HashMap;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+
+
+class Outtake extends SequentialCommandGroup {
+
+    BallGrabber ballGrabber;
+  
+  public Outtake(BallGrabber ballGrabber) {
+    this.ballGrabber = ballGrabber;
+    addCommands(
+        Outtake()
+    );
+  } 
+  public Command Outtake() {
+    if(ballGrabber.hasBall())
+    return new RunCommand(() -> ballGrabber.runOuttake(), ballGrabber
+    );
+    else{
+        return null;
+    }
+  }
+  }
+
+
+
+
+
